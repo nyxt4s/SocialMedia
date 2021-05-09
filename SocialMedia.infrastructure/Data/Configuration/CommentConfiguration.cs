9c3e8f9b2c9905b1e7047cef2923c30acdd8e238
RODERICK LUIS MEDINA GARAY <RODERICK.MEDINA@correoaiep.cl>
2021-05-08 23:12:01 -0400

    se agrega busca por id en las peticiones, buenas practicas para el mapeo de las tables (dto)

---
+++ SocialMedia.infrastructure/Data/Configuration/CommentConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Data.Configuration
+{
+    class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.ToTable("Comentario");
+
+            builder.HasKey(e => e.CommentId);
+
+            builder.Property(e => e.CommentId)
+            .HasColumnName("IdComentario")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.IdPost)
+            .HasColumnName("IdPublicacion")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.UserID)
+            .HasColumnName("IdUsuario")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.IsActive)
+            .HasColumnName("Activo")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.Description)
+                .IsRequired()
+                .HasColumnName("Descripcion")
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+            builder.Property(e => e.Date)
+            .HasColumnName("Fecha")
+            .HasColumnType("datetime");
+
+            builder.HasOne(d => d.Post)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(d => d.IdPost)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Comentario_Publicacion");
+
+            builder.HasOne(d => d.User)
+                .WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserID)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Comentario_Usuario");
+        }
+    }
+}
---
+++ SocialMedia.infrastructure/Data/Configuration/PostConfiguration.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infrastructure.Data.Configuration
+{
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        //recibimos el metodo de la interfaz
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasKey(e => e.PostId);
+
+            builder.ToTable("Publicacion");
+
+            builder.Property(e => e.PostId)
+            .HasColumnName("IdPublicacion")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.UserId)
+            .HasColumnName("IdUsuario")
+            .ValueGeneratedNever();
+
+            builder.Property(e => e.Description)
+                .HasColumnName("Descripcion")
+                .IsRequired()
+                .HasMaxLength(1000)
+                .IsUnicode(false);
+
+            builder.Property(e => e.Date)
+                .HasColumnName("Fecha")
+                .HasColumnType("datetime");
+
+            builder.Property(e => e.Image)
+                .HasColumnName("Imagen")
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+            builder.HasOne(d => d.User)
+                .WithMany(p => p.Post)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Publicacion_Usuario");
+        }
+    }
+}
